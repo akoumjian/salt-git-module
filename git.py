39480f5570d1a6aca7ccea24b5209cdf0f004b60
Alec Koumjian <akoumjian@gmail.com>
2012-02-06 11:41:49 -0800

    Barebones git clone

---
+++ git.py
@@ -0,0 +1,24 @@
+'''
+Clone git repositories.
+'''
+
+
+def clone(repo='', dest=''):
+    '''
+    Clone a git repository
+
+    repo : None
+        The repository source.
+    dest : None
+        The destination directory. Uses the default from git if none specified
+
+    CLI Example::
+
+        salt '*' git.clone git@github.com:username/myrepo.git \\
+                /path/to/destination/myrepo
+    '''
+    cmd = 'git clone {repo} {dest}'.format(
+        repo=repo,
+        dest=dest)
+
+    return __salt__['cmd.run'](cmd)
